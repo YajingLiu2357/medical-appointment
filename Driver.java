c11c8180fe921afb839e8a623d95edc30f110d7e
YajingLiu2357 <79319316+YajingLiu2357@users.noreply.github.com>
2024-02-12 19:33:42 -0500

    delete unused code

--- Driver.java
+++ Driver.java
@@ -2,17 +2,6 @@ import java.util.Scanner;
 
 public class Driver {
     public static void main(String[] args) throws Exception{
-//        AdminClient adminClientMTL = new AdminClient("MTLA0001");
-//        adminClientMTL.addAppointment("MTLA080224", "Physician", 2);
-//        AdminClient adminClientQUE = new AdminClient("QUEA0001");
-//        adminClientQUE.addAppointment("QUEA080224", "Physician", 2);
-//        adminClientQUE.addAppointment("QUEM080224", "Surgeon", 2);
-//        adminClientMTL.listAppointmentAvailability("Physician");
-//        //adminMTL.removeAppointment("MTLA080224", "Physician");
-//        PatientClient patientClientMTL = new PatientClient("MTLP0001");
-//        patientClientMTL.bookAppointment("MTLP0001", "MTLA080224", "Physician");
-//        patientClientMTL.getAppointmentSchedule("MTLP0001");
-//        patientClientMTL.cancelAppointment("MTLP0001", "MTLA080224");
 
         System.out.println("Please enter your ID:");
         Scanner scanner = new Scanner(System.in);
